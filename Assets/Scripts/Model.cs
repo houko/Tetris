c38a7f5560723ba2bba865b0690ebfc96dba7dc9
hupeng <xiaomo@xiaomo.info>
2018-11-02 09:37:04 +0800

    model

--- Assets/Scripts/Model.cs
+++ Assets/Scripts/Model.cs
@@ -7,15 +7,28 @@ using UnityEngine;
 *  github : https://github.com/xiaomoinfo
 *  描   述 ：模型
 */
-public class Model : MonoBehaviour {
+public class Model : MonoBehaviour
+{
+    public Transform[,] map = new Transform[10, 23];
 
-	// Use this for initialization
-	void Start () {
-		
-	}
 
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+
+    public bool IsValidMapPosition(Transform t)
+    {
+//        foreach (var VARIABLE in t.)
+//        {
+//            
+//        }
+        return false;
+    }
 }
