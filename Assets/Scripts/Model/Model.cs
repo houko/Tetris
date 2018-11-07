c689ac88efe1f54b4d054f00bcf39a7fa59fab9a
hupeng <xiaomo@xiaomo.info>
2018-11-07 17:01:52 +0800

    判断格子是否可用

--- Assets/Scripts/Model/Model.cs
+++ Assets/Scripts/Model/Model.cs
@@ -9,23 +9,51 @@ using UnityEngine;
 */
 public class Model : MonoBehaviour
 {
-    public Transform[,] map = new Transform[10, 18];
+    private const int column = 10;
+    private const int row = 18;
+    public Transform[,] map = new Transform[column, row];
 
 
+    /// <summary>
+    /// 地图是否可用
+    /// </summary>
+    /// <param name="t"></param>
+    /// <returns></returns>
     public bool IsValidMapPosition(Transform t)
     {
         foreach (Transform child in t)
         {
-            if (child.CompareTag("Block"))
+            // 非block不能放入地图
+            if (!child.CompareTag("Block"))
             {
-                Vector2 pos = child.position.Round();
-                if (map[(int) pos.x, (int) pos.y] == null)
-                {
-                    
-                }
+                continue;
+            }
+
+            // 格子不能重叠
+            Vector2 pos = child.position.Round();
+            if (map[(int) pos.x, (int) pos.y] != null)
+            {
+                return false;
+            }
+
+            // 格子不能超出地图范围
+            if (isInsideMap(pos))
+            {
+                return false;
             }
         }
 
         return false;
+    }
+
+
+    /// <summary>
+    /// 是否在地图内
+    /// </summary>
+    /// <param name="pos"></param>
+    /// <returns></returns>
+    public bool isInsideMap(Vector2 pos)
+    {
+        return pos.x >= 0 && pos.y > 0 && pos.x < column && pos.y < row;
     }
 }
